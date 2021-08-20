84b5020e6a85a5000fb99b03a15cd3e125e0d43d
Lei Xiao <40417423+xiaol-arch@users.noreply.github.com>
2021-08-20 14:35:04 +0800

    Add files via upload

--- distance.py
+++ distance.py
@@ -1,4 +1,6 @@
 import cv2
+import queue
+import threading
 import sys
 import time
 from pathlib import Path
@@ -36,6 +38,29 @@ def init():
     _ = model(img01.half() if half else img01) if device.type != 'cpu' else None  # run once
     return device, half, model, names, colors
 
+def Receive():
+    print("start Receive");
+    cap = cv2.VideoCapture("rtsp://admin:admin1234@192.168.1.108/cam/realmonitor?channel=1&subtype=0");
+    ret, frame = cap.read();
+    q.put(frame);
+    while ret:
+        ret, frame = cap.read();
+        q.put(frame);
+
+def Display(device, half, model, names, colors):
+    print("start displaying");
+    window_name = "192.168.1.108";
+    cv2.namedWindow(window_name, flags = cv2.WINDOW_AUTOSIZE);
+    while True:
+        if q.empty() != True:
+            frame = q.get();
+            imgs = [frame]
+            img, pred = predict_img(imgs, device, half, model)
+            v1 = draw_img_info(pred, imgs, img, names, colors)
+            cv2.imshow(window_name, v1);
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
 def predict_img(imgs, device, half, model):
     img = [letterbox(x, new_shape=640, auto=True)[0] for x in imgs]
     # Stack
@@ -57,6 +82,31 @@ def predict_img(imgs, device, half, model):
     pred = non_max_suppression(pred, 0.25, 0.45, classes=[0, 1, 2, 3, 5, 6, 7], agnostic=False)
     return img, pred
 
+def draw_img_info(pred, imgs, img, names, colors):
+    for i, det in enumerate(pred):  # detections per image
+        s, im0 =  '%g: ' % i, imgs[i].copy()
+        s += '%gx%g ' % img.shape[2:]  # print string
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+            for *xyxy, conf, cls in reversed(det):
+                distance = 0
+                if names[int(cls)] == 'person':
+                    object_width_in_frame = int(xyxy[2]) - int(xyxy[0])
+                    distance = distance_finder(focal_length_person, object_width_in_frame, KNOWN_PERSON_WIDTH)
+                elif names[int(cls)] == 'bus':
+                    object_width_in_frame = int(xyxy[2]) - int(xyxy[0])
+                    distance = distance_finder(focal_length_person, object_width_in_frame, KNOWN_BUS_WIDTH)
+                elif names[int(cls)] == 'car':
+                    object_width_in_frame = int(xyxy[2]) - int(xyxy[0])
+                    distance = distance_finder(focal_length_person, object_width_in_frame, KNOWN_CAR_WIDTH)
+                elif names[int(cls)] == 'motorcycle':
+                    object_width_in_frame = int(xyxy[2]) - int(xyxy[0])
+                    distance = distance_finder(focal_length_person, object_width_in_frame, KNOWN_MOTORCYCLE_WIDTH)
+                label = f'{names[int(cls)]} {conf:.2f} {distance:.3f}m'
+                plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+    return im0
+
 def ref_img_information(img_path, device, half, model):
     imgs = [cv2.imread(img_path)]
     img, pred = predict_img(imgs, device, half, model)
@@ -70,19 +120,18 @@ def ref_img_information(img_path, device, half, model):
                 if names[int(cls)] == 'person':
                     ref_image_object_width = int(xyxy[2]) - int(xyxy[0])
                     focal_length_found = focal_length(ref_image_object_width, KNOWN_PRESON_DISTANCE, KNOWN_PERSON_WIDTH)
-                elif (names[int(cls)] == 'bus') and (round(float(conf), 2)==0.61):
+                elif (names[int(cls)] == 'bus') and (round(float(conf), 2) == 0.61):
                     ref_image_object_width = (int(xyxy[2]) - 400) - int(xyxy[0])
                     focal_length_found = focal_length(ref_image_object_width, KNOWN_BUS_DISTANCE, KNOWN_BUS_WIDTH)
-                elif (names[int(cls)] == 'car') and (round(float(conf), 2)==0.47):
+                elif (names[int(cls)] == 'car') and (round(float(conf), 2) == 0.47):
                     ref_image_object_width = (int(xyxy[2]) - 30) - int(xyxy[0])
                     focal_length_found = focal_length(ref_image_object_width, KNOWN_CAR_DISTANCE, KNOWN_CAR_WIDTH)
-                elif (names[int(cls)]=='motorcycle'):
+                elif (names[int(cls)] == 'motorcycle'):
                     ref_image_object_width = (int(xyxy[2])) - int(xyxy[0])
                     focal_length_found = focal_length(ref_image_object_width, KNOWN_MOTORCYCLE_DISTANCE, KNOWN_MOTORCYCLE_WIDTH)
     return focal_length_found
 
-if __name__ == '__main__':
-    device, half, model, names, colors = init()
+def get_focal_length(device, half, model):
     bus_img_path = 'data/images/Ref_bus.jpg'
     car_img_path = 'data/images/Ref_car.jpg'
     motorcycle_img_path = 'data/images/Ref_motorcycle.jpg'
@@ -92,49 +141,15 @@ if __name__ == '__main__':
     focal_length_car = ref_img_information(car_img_path, device, half, model)
     focal_length_motorcycle = ref_img_information(motorcycle_img_path, device, half, model)
     focal_length_person = ref_img_information(person_img_path, device, half, model)
+    return focal_length_bus, focal_length_car, focal_length_motorcycle, focal_length_person
 
-    video_path = 'data/video/testVideo.mp4'
-    save_path = 'data/video/videoResult.mp4'
-    cap = cv2.VideoCapture(video_path)
-    assert cap.isOpened(), f'Failed to open {video_path}'
-    # get video information
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-    ret, frame = cap.read()
-    while(ret):
-        imgs = [frame]
-        img, pred = predict_img([frame], device, half, model)
-        for i, det in enumerate(pred):  # detections per image
-            s, im0 =  '%g: ' % i, imgs[i].copy()
-            s += '%gx%g ' % img.shape[2:]  # print string
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-                for *xyxy, conf, cls in reversed(det):
-                    distance = 0
-                    if names[int(cls)] == 'person':
-                        object_width_in_frame = int(xyxy[2]) - int(xyxy[0])
-                        distance = distance_finder(focal_length_person, object_width_in_frame, KNOWN_PERSON_WIDTH)
-                    elif names[int(cls)] == 'bus':
-                        object_width_in_frame = int(xyxy[2]) - int(xyxy[0])
-                        distance = distance_finder(focal_length_person, object_width_in_frame, KNOWN_BUS_WIDTH)
-                    elif names[int(cls)] == 'car':
-                        object_width_in_frame = int(xyxy[2]) - int(xyxy[0])
-                        distance = distance_finder(focal_length_person, object_width_in_frame, KNOWN_CAR_WIDTH)
-                    elif names[int(cls)] == 'motorcycle':
-                        object_width_in_frame = int(xyxy[2]) - int(xyxy[0])
-                        distance = distance_finder(focal_length_person, object_width_in_frame, KNOWN_MOTORCYCLE_WIDTH)
-                    label = f'{names[int(cls)]} {conf:.2f} {distance:.3f}m'
-                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-        cv2.imshow('a', im0)
-        cv2.waitKey(20)
-        # transform frame to video
-        vid_writer.write(im0)
-        ret, frame = cap.read()
-    cap.release()
-    cv2.destroyAllWindows()
+q = queue.Queue()
+device, half, model, names, colors = init()
+focal_length_bus, focal_length_car, focal_length_motorcycle, focal_length_person = get_focal_length(device, half, model)
 
+if __name__ == '__main__':
+    p1 = threading.Thread(target = Receive);
+    p2 = threading.Thread(target = Display, args=(device, half, model, names, colors));
 
-
+    p1.start();
+    p2.start();
